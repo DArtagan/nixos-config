3fbb11906d65ac910c6fc5fc9d6601b2815a5313
William Weiskopf <william@weiskopf.me>
2024-07-14 12:48:24 -0400

    Steam Deck: configuration.nix flake, jovian steamos, home-manager.

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    jovian-nixos = {
+      url = "github:Jovian-Experiments/Jovian-NixOS/development";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs@{ self, nixpkgs, jovian-nixos, ... }: {
+    nixosConfigurations.nix-steam-deck = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.willy = import ./home.nix;
+        }
+        jovian-nixos.nixosModules.default {
+          jovian = {
+            steam.enable = true;
+            devices.steamdeck = {
+              enable = true;
+            };
+          };
+        }
+      ];
+    };
+  };
+}
