7fa06054449f7a4004ed43dc35c12922b6961774
William Weiskopf <william@weiskopf.me>
2024-10-13 22:25:14 -0400

    Tmux configuration.

--- home.nix
+++ home.nix
@@ -103,26 +103,86 @@
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
+      shell = {
+        args = ["-l" "-c" "${pkgs.tmux}/bin/tmux"];
+        program = "${pkgs.fish}/bin/fish";
+      };
     };
   };
 
-  #programs.bash = {
-  #  enable = true;
-  #  enableCompletion = true;
-  #  # TODO add your custom bashrc here
-  #  bashrcExtra = ''
-  #    export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-  #  '';
-
-  #  # set some aliases, feel free to add more or remove some
-  #  shellAliases = {
-  #    urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-  #    urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-  #  };
-  #};
-
   programs.fish = {
     enable = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    shortcut = "a";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    extraConfig = ''
+      # Bind "prefix" for summoning tmux to Ctrl-a
+      set-option -g prefix C-a
+      
+      # We're 256 color ready
+      set -g default-terminal "screen-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      
+      # Turn on mouse mode
+      set -g mouse on
+      
+      # Start numbering windows from 1
+      set -g base-index 1
+      
+      # Set the Window titles based on what's open in tmux
+      set-option -g set-titles on
+      
+      # Increase scrollback history limit
+      set-option -g history-limit 5000
+      
+      # reload config file
+      bind r source-file ~/.tmux.conf
+      
+      
+      # Pane navigation
+      ### Consider: tmux-pain-control plugin instead
+      ## pane_navigation_bindings
+      bind h   select-pane -L
+      bind C-h select-pane -L
+      bind j   select-pane -D
+      bind C-j select-pane -D
+      bind k   select-pane -U
+      bind C-k select-pane -U
+      bind l   select-pane -R
+      bind C-l select-pane -R
+      
+      ## window_move_bindings
+      bind -r "<" swap-window -t -1
+      bind -r ">" swap-window -t +1
+      
+      ## pane_resizing_bindings
+      bind -r H resize-pane -L 2
+      bind -r J resize-pane -D 2
+      bind -r K resize-pane -U 2
+      bind -r L resize-pane -R 2
+      
+      ## pane_split_bindings
+      bind "|" split-window -h -c "#{pane_current_path}"
+      bind "\\" split-window -fh -c "#{pane_current_path}"
+      bind "-" split-window -v -c "#{pane_current_path}"
+      bind "_" split-window -fv -c "#{pane_current_path}"
+      bind "%" split-window -h -c "#{pane_current_path}"
+      bind '"' split-window -v -c "#{pane_current_path}"
+      
+      ## improve_new_window_binding
+      bind "c" new-window -c "#{pane_current_path}"
+      
+      # copy/paste
+      set-window-option -g mode-keys vi
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+    '';
   };
 
   # This value determines the home Manager release that your
