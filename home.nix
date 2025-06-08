fca610b50a965e9f7ef78e10be2813bb8d509231
William Weiskopf <william@weiskopf.me>
2025-06-08 17:49:13 -0400

    Talosctl.

--- home.nix
+++ home.nix
@@ -69,6 +69,7 @@
     ripgrep  # recursively searches directories for a regex pattern
     strace  # system call monitoring
     sysstat
+    talosctl
     texlive.combined.scheme-medium
     tree
     unzip
